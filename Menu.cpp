2295f28d00acaf4844b90328a06deebc22b140d1
Elizabeth Donato <donatoe@oregonstate.edu>
2018-02-10 23:23:16 -0600

    Create Menu.cpp
    This is the header file for a simple menu function that allows for 2 versions of a menu to be displayed - 1 for the initial time the program is run and 1 for all other subsequent executions.  It also validates the user's input.

---
+++ Menu.cpp
@@ -0,0 +1,83 @@
+/************************************************
+ * Program Name: menu.cpp
+ * Author: Elizabeth Donato
+ * Date: 1-20-2018
+ * Description: This is the implementation file
+   for the menu class  This class can
+   easily be modified for use by different
+   programs.
+************************************************/
+
+#include "Menu.hpp"
+#include "ValidInt.hpp"
+#include <iostream>
+
+/*************************************************
+ *              Menu::Menu                      *
+ *                                              *
+ * This is the constructor for the Menu class.  *
+ * It initializes class variables.              *
+ *                                              *
+ * Accepts: Nothing                             *
+ *                                              *
+ * Returns: Nothing                             *
+*************************************************/
+Menu::Menu()
+{
+   firstMenu = true;            //True if this is the first time user is seeing the menu
+   testString = "";
+   validInput = false;          //Will change to true once valid input has been entered
+   lowest = 1;
+   highest = 2;
+}
+
+/*************************************************
+ *              Menu::get_choice                *
+ *                                              *
+ *                                              *
+ * This function displays a menu of choices     *
+ * for the user and gets the user's choice.     *
+ *                                              *
+ * Accepts: bool (Determines if this is the     *
+ *          first time the program has been     *
+ *          run)                                *
+ *                                              *
+ * Returns: int (User's choice)                 *
+ *                                              *
+*************************************************/
+int Menu::get_choice()
+{
+   //Gets and validates user's choice
+   while(!validInput)
+   {
+        //If this is the first time the menu is being displayed
+        if(firstMenu == true)
+        {
+           std::cout << "\n\n  Program Menu";
+           std::cout << "\n--------------------";
+           std::cout << "\n1.  Start Program";
+           std::cout << "\n2.  Quit Program";
+        }
+
+        //If the simulation has already been run at least once
+        else
+        {
+           std::cout << "\n\n  Program Menu";
+           std::cout << "\n--------------------";
+           std::cout << "\n1.  Play Again";
+           std::cout << "\n2.  Quit Program";
+        }
+
+        std::cout << "\n\nPlease enter your choice:  ";
+        std::getline(std::cin, testString);
+        validInput = userInput.is_valid_int_range(testString, lowest, highest);
+   }
+
+   //Sets boolean value so indicate the user has already executed the program once
+   firstMenu = false;
+
+   //Returns user's choice to client program
+   userChoice = userInput.get_int();
+
+   return userChoice;
+}
