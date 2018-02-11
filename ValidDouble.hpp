5f9df8767ade9a69e5b3d118d5b8262f846b399b
Elizabeth Donato <donatoe@oregonstate.edu>
2018-02-10 21:40:49 -0600

    Create ValidDouble.hpp
    Header file for double input validation class ValidDouble.

---
+++ ValidDouble.hpp
@@ -0,0 +1,28 @@
+/************************************************
+ * Program Name: ValidDouble.hpp
+ * Author: Elizabeth Donato
+ * Date: 2-9-2018
+ * Description: This is the header file
+ *              for the ValidDouble class.
+*************************************************/
+
+#ifndef VALIDDOUBLE_HPP
+#define VALIDDOUBLE_HPP
+
+#include <string>
+
+//ValidDouble Class Declaration
+class ValidDouble
+{
+   private:
+        bool goodInput;
+        double testDouble;
+
+   public:
+        ValidDouble();
+        bool is_valid_double(std::string);
+        bool is_valid_double_range(std::string, double, double);
+        ~ValidDouble() {};
+};
+
+#endif
